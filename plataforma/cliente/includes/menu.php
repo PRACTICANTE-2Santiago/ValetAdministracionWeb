6fce4a9bfcbe6e89292282d26a53eea7cfd7951c
PRACTICANTE-2Santiago <practicante2@linkom.mx>
2022-03-10 15:09:49 -0600

    onceavo commit

---
+++ plataforma/cliente/includes/menu.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+
+<ul class="nav nav-sidebar">
+
+    <li>
+
+        <div class="accordion" id="accordionDatos">
+            <div class="accordion-group">
+                <div class="accordion-heading" href="#collapseDatos" data-toggle="collapse" data-parent="#accordionDatos">
+                    <a class="accordion-toggle">
+                        Datos <span class="caret"></span>
+                    </a>
+                </div>
+                <div class="accordion-body collapse <?php echo isset($pa) ? $pa == 'comunidad' ? 'in': '' :''; ?>" id="collapseDatos">
+
+
+                    <a href="usuarios.php?esta=1">
+                        <div class="accordion-inner">Usuarios</div>
+                    </a>
+
+
+
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+    <li><a href="https://linkom.mx" target="_blank">Powered by LINKOM &copy; <?php echo date('Y')?></a></li>
+</ul>
